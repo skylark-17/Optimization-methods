4b64090268be41623746f3f7946d95ad9e630775
skylark-17 <zhavoronkov.na@phystech.edu>
2022-11-12 00:24:33 +0300

    added plotting

---
+++ plotting.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+from brents_method import one_dim, one_dim_with_derivative
+
+
+def make_plot(a, c, f, df=None):
+    if df is None:
+        res = one_dim(a, c, f)
+    else:
+        res = one_dim_with_derivative(a, c, f, df)
+    points = res.points
+    values = res.values
+    x0 = res.x
+    fx0 = res.fx
+    grid = np.linspace(a, c, 1000)
+    plt.plot(grid, f(grid), color='blue')
+    plt.plot(points, values, color='red')
+    plt.scatter(points, values, color='black')
+    plt.title("Brents method" if df is None else "Brents method with derivative")
+    plt.xlabel(f'$x$')
+    plt.ylabel(f'$f(x)$')
+    plt.show()
